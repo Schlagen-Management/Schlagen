bd8204cc95dfaec8d449dcdbcb97e35de5f79fef
Todd Littlejohn <littlejohnjt@hotmail.com>
2020-03-20 17:33:47 -0400

    more commenting out so stefano can use VS for mac

--- Schlagen/Startup.cs
+++ Schlagen/Startup.cs
@@ -33,7 +33,7 @@ namespace Schlagen
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddApplicationInsightsTelemetry();
+            //services.AddApplicationInsightsTelemetry();
 
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
